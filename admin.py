400f4af5ba8b808337d3f21525b80def2c6e9c8d
Gokul Palanisamy <gokulajith80@gmail.com>
2024-05-29 11:27:05 -0400

    New changes has been added

--- admin.py
+++ admin.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from langchain_community.llms import GooglePalm
-from langchain_community.utilities import SQLDatabase
+from langchain.llms import GooglePalm
+from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.prompts import SemanticSimilarityExampleSelector
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.prompts import FewShotPromptTemplate
 from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
 from langchain.prompts.prompt import PromptTemplate
